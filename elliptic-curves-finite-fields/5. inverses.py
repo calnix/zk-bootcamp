406e612214267f71f8d015ce14a9ee7443894907
calnix <calnix.289@gmail.com>
2023-10-27 08:06:29 +0800

    inverses

--- elliptic-curves-finite-fields/5. inverses.py
+++ elliptic-curves-finite-fields/5. inverses.py
@@ -74,5 +74,6 @@ assert (int(P[1]) + int(P1[1])) == field_modulus, "y-values must add up to field
 
 
 # Point addition of P and its inverse will return the identity point: point at infinity
-#
+# Adding the y-coordinates will return the field modulus
+
 
